c1004ab03cfc7063efd0d2a312cc8b1cbbfa03ec
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-09-08 09:17:01 +0530

    new:added the whole project

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import LobbyScreen from "./screens/Lobby";
+import RoomPage from "./screens/Room";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LobbyScreen />} />
+        <Route path="/room/:roomId" element={<RoomPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { SocketProvider } from "./context/SocketProvider";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
